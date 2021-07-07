668d75d87188bd5f6cd0e1cdca606f826f2dbd1d
liuyuefeilengxue <1365119439@qq.com>
2021-07-07 23:09:16 +0800

    20210707-10:44

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-a = input()
-print(a)
+# -*- coding: utf-8 -*-
+# list 注意，list里面是可以不同的数据类型
+student_name=['zhangsan','lisi',12,16]
+print(student_name[0])
+print(student_name[2])
 
-print("what the fuck")
+# tuple 元组，一旦初始化，就不能修改
+t = ('zhangsan','lisi',123,565)
+print(t[1])
+
+# dict
